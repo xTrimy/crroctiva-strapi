c54ac6392d867176b6c5e59374d7898cf0bb9e8a
xTrimy <Mohamed.ashraf881999@gmail.com>
2025-04-05 01:38:59 +0200

    Enhance security middleware with content security policy configuration

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,18 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'http:', 'https:'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
